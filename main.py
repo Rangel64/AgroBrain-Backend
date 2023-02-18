4367dd52595524dfb2a751bb2a7a4153a83be4f8
Rangel64 <63917837+Rangel64@users.noreply.github.com>
2023-02-18 12:08:05 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
-from flask import Flask, jsonify
-import os
+from flask import Flask, request, jsonify
+import json
+import base64
+import Estimador
 
 app = Flask(__name__)
 
+@app.route('/', methods = ['GET','POST'])
 
-@app.route('/')
+
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    global reponse
+    
+    if(request.method == 'POST'):
+        request_data = request.data
+        request_data = json.loads(request_data.decode('utf-8'))
+        decodeString = request_data['image']
+        imageBytes = base64.b64decode((decodeString))
+        numeroDeAnimais = Estimador.result(imageBytes)
+        return jsonify({'response': str(numeroDeAnimais)})
+    
+    return "<h1>Hello World</h1>"  
+        
+if(__name__ == "__main__"):
+    app.run(host = 'localhost',debug=True)
 
-
-if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
