4beb4d978c0010236af5f9fee45bf96b3abc08be
Rangel64 <rangersouza35@gmail.com>
2023-03-03 23:32:03 -0300

    add rede r2_score: 0.836

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ if(__name__ == "__main__"):
     url = 'https://drive.google.com/file/d/1mhk3M-qOR2tr22HHMe5ajKbl7k8EeUa2/view?usp=sharing'
     output = 'models/model_teste_7_keras_nadam_11relu_linear_1200neur_Dropout_0.100000_100000epocs_patience_150.h5'
     gdown.download(url, output, quiet=False, fuzzy=True)
-    
+    for i in range(30):
+        print('douglas cabaço')
     app.run(host = 'localhost',port=5000,debug=True)
 
