eb5c99f341545c22f21c5d9bb61baca526af983a
Rangel64 <rangersouza35@gmail.com>
2023-03-03 22:20:06 -0300

    rede r2_score: 0.836

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import Flask, request, jsonify
 import json
 import base64
 import Estimador
+import gdown
+
 
 app = Flask(__name__)
 
@@ -22,5 +24,8 @@ def index():
     return "<h1>Hello World</h1>"  
         
 if(__name__ == "__main__"):
+    url = 'https://drive.google.com/file/d/1mhk3M-qOR2tr22HHMe5ajKbl7k8EeUa2/view?usp=share_link'
+    output = 'models/model_teste_7_keras_nadam_11relu_linear_1200neur_Dropout_0.100000_100000epocs_patience_150.h5'
+    gdown.download(url, output, quiet=False)
     app.run(host = '0.0.0.0',port=5000,debug=True)
 
