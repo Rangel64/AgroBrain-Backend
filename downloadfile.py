67d86116cd5ca89c1847d2e3f00a5397007d704f
Rangel64 <rangersouza35@gmail.com>
2024-02-27 12:05:30 -0300

    update

--- downloadfile.py
+++ downloadfile.py
@@ -1,7 +1,7 @@
 import gdown
 
-url = 'https://drive.google.com/file/d/1eWicqYjGdp9IXBtZM0xD1zH-a5KHt2UD/view?usp=sharing'
-output = 'models'
+url = 'https://drive.google.com/file/d/1cNfWe-F1cyN2Fcu7l9pXNFeVnhKKl09I/view?usp=sharing'
+output = 'models/model.h5'
 gdown.download(url, output, quiet=False, fuzzy=True)
 
 for i in range(10):
